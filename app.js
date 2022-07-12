d0b1348b47f9f03f8d016af11f997d492bb9ff74
saikumar <saikumar.19@mountblue.tech>
2022-07-12 19:12:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var signUp = angular.module('signUp', []);
+signUp.controller('signUpController', ['$scope', function ($scope) {
+  $scope.information = {}
+  $scope.submitForm = function (user) {
+    $scope.msg = "You are successfully logged in " + $scope.user.firstName;
+    console.log($scope.information.firstName)
+    $scope.information = angular.copy(user)
+  }
+}])
